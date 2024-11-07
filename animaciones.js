df59f5197ff3df31ab3b6a0000fe78eec10c3517
maruyanx <kidmaruyan@gmail.com>
2024-11-07 13:37:10 -0300

    Add files via upload

--- animaciones.js
+++ animaciones.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const title = document.getElementById("title");
     const text = title.innerText;
-    title.innerText = ''; // Vaciar el título inicial
+    title.innerText = ''; 
 
     let i = 0;
     function typeLetter() {
         if (i < text.length) {
             title.innerText += text[i];
             i++;
-            setTimeout(typeLetter, 150); // Ajusta la velocidad de la animación
+            setTimeout(typeLetter, 150); 
         }
     }
-
-    // Iniciar la animación de las letras al cargar la página
     setTimeout(typeLetter, 500);
 
-    // Animación para el título
     title.style.animation = 'fadeIn 1s ease forwards, slideIn 1s ease forwards';
 });
